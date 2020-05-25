ba458e0bf61628d0e4694e072470a92f7c1682b9
lebohang0824 <lebohang0824@gmail.com>
2020-05-25 15:03:02 +0200

    Update instructions

--- src/components/quiz/header/QuizInstructions.vue
+++ src/components/quiz/header/QuizInstructions.vue
@@ -3,7 +3,8 @@
         <h1>Instructions</h1>
         <p>
             This Quiz Game is crafted using <b>Vue.js</b> and <b>Trivia API</b>.<br> Answer each question
-            by making selection of the correct answer provided below the question. This game has 15 questions,
+            by making selection of the correct answer provided below the question. This game has 15 questions, and 10 seconds
+            to answer each question.
             All the best! <i>Show us what you got!!!</i><br>
 
             <a href="https://github.com/lebohang0824/gm-quiz-game">Source</a>
@@ -32,6 +33,9 @@ export default {
         margin-top: 10px;
         border-radius: 50%;
         background-color: rgba(0, 0, 0, .1);
+    }
+    p {
+        font-size: 14px;
     }
     img {
         padding-top: 2px;
