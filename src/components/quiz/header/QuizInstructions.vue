3d794c862aff6c4ebf1166906e6e62b3859bc7eb
lebohang0824 <lebohang0824@gmail.com>
2020-05-25 14:34:37 +0200

    Add languages

--- src/components/quiz/header/QuizInstructions.vue
+++ src/components/quiz/header/QuizInstructions.vue
@@ -2,8 +2,15 @@
     <div id="quiz-instructions">
         <h1>Instructions</h1>
         <p>
-            This Quiz Game was developed using <b>Vue.js</b> and <b>Trivia API</b>
-        </p> 
+            This Quiz Game is crafted using <b>Vue.js</b> and <b>Trivia API</b>.<br> Answer each question
+            by making selection of the correct answer provided below the question. This game has 15 questions,
+            All the best! <i>Show us what you got!!!</i><br>
+
+            <a href="https://github.com/lebohang0824/gm-quiz-game">Source</a>
+        </p>
+        <div class="vue">
+            <img src="../../../assets/logo.png" alt="Vue" />
+        </div> 
     </div>
 </template>
 
@@ -17,5 +24,18 @@ export default {
 <style scoped>
     #quiz-instructions {
         padding-top: 20px;
+    }
+    .vue {
+        width: 25px;
+        height: 25px;
+        padding: 8px;
+        margin-top: 10px;
+        border-radius: 50%;
+        background-color: rgba(0, 0, 0, .1);
+    }
+    img {
+        padding-top: 2px;
+        height: 100%;
+        width: auto;
     }
 </style>
